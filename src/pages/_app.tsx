6d7a29ba32f37e57a917ea6dc6377faa4fdffe04
uewtwo <nkyt161@gmail.com>
2024-02-02 13:23:38 +0900

    add vercel seepinsight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { appWithTranslation } from "next-i18next"
 import type { AppProps } from "next/app"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@hiatus/styles/globals.css"
 
 export function App({ Component, pageProps }: AppProps) {
@@ -8,6 +9,7 @@ export function App({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
