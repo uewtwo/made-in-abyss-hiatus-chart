87d2ad15b90436a944acfe52af33be3de13ce318
uewtwo <nkyt161@gmail.com>
2024-02-04 06:30:14 +0900

    google site verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="i24iU-1pRkcrNji8sEZUTpCsoLueFJoXPaG5n-XiZ_I"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
