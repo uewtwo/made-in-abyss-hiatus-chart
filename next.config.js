5f92dcac1075ade988d83d78f25065ae2c49a6e0
uewtwo <nkyt161@gmail.com>
2024-02-04 18:21:53 +0900

    enable pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const { i18n } = require("./next-i18next.config")
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NEXT_PUBLIC_ENV === "develop",
+})
 
-module.exports = {
+module.exports = withPWA({
   i18n,
   reactStrictMode: true,
-}
+})
